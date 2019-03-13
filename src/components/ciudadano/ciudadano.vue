cd6bd6bc4e8d6c3f94a272945b8fdb25848b3849
alvaro.recalde.alguacil <elvarox301@gmail.com>
2019-03-13 14:35:24 +0100

    mostrar datos de ciudadano en ciudadano.vue

--- src/components/ciudadano/ciudadano.vue
+++ src/components/ciudadano/ciudadano.vue
@@ -1,19 +1,33 @@
 <template>
-    <div id="ciudadano">
-      <h1>{{ datos }}</h1>
+    <div id="ciudadano" v-if="ciudadano">
+      <h3>Nombre: {{ ciudadano.nombre }}</h3>
+      <h3>Apellidos: {{ ciudadano.apellidos }}</h3>
+      <h3>DNI: {{ ciudadano.dni }}</h3>
+      <h3>Genero: {{ ciudadano.genero }}</h3>
+      <h3>Edad: {{ ciudadano.edad }}</h3>
     </div>
 </template>
 
 <script>
+  import db from '../../firebase/init';
     export default {
         name: "ciudadano",
       data(){
           return {
-            datos: null
+            datos: null,
+            ciudadano: null
           }
       },
       created() {
-          this.datos = this.$route.params.id_ciudadano;
+          this.datos = this.$route.params.id_ciudadano
+        let ref = db.collection('ciudadano').where('usuario', '==', this.$route.params.id_ciudadano);
+        ref.get().then(snapshot => {
+          snapshot.forEach(doc => {
+            this.ciudadano = doc.data();
+            this.ciudadano.id = doc.id;
+            console.log(this.ciudadano);
+          })
+        });
       }
     }
 </script>
